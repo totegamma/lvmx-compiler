d478ea99bb4c0ca9e873d8b23f386016bbad40ae
thotgamma <github@gammalab.net>
2020-11-12 00:04:50 +0900

    AST作るところまで

---
+++ lex.py
@@ -0,0 +1,110 @@
+import ply.lex as lex
+
+from tokens import tokens
+
+
+reserved = {
+        'func': 'FUNC',
+        'var': 'VAR',
+        'if': 'IF',
+        'then': 'THEN',
+        'else': 'ELSE',
+        'while': 'WHILE',
+        'for': 'FOR',
+        'input': 'INPUT',
+        'output': 'OUTPUT',
+        'return': 'RETURN'
+}
+
+states = (
+        ('string', 'exclusive'),
+        ('comment', 'exclusive')
+)
+
+t_ASSIGN = r'='
+t_COLON = r':'
+t_SEMICOLON = r';'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_LBRACE = r'\{'
+t_RBRACE = r'\}'
+t_COMMA = r','
+
+t_NUMBER = r'[0-9]+(\.[0-9]+)?((e|E)(\+|-)?[0-9]+)?'
+
+def t_UNIOP(t):
+    r'\+\+|--|\!'
+    return t
+
+def t_BIOP(t):
+    r'\+|-|\*|/|<|<=|>|>=|==|\!='
+    return t
+
+
+def t_SYMBOL(t):
+    r'[a-zA-Z][a-zA-Z0-9]*'
+    t.type = reserved.get(t.value, 'SYMBOL')
+    return t
+
+def t_begin_string(t):
+    r'"'
+    t.lexer.push_state('string')
+
+def t_string_STRING(t):
+    r'[^"]+'
+    return t
+
+def t_string_end(t):
+    r'"'
+    t.lexer.pop_state()
+
+def t_begin_comment(t):
+    r'/\*'
+    t.lexer.push_state('comment')
+
+def t_comment_COMMENT(t):
+    r'[^\*/]+'
+    pass
+
+def t_comment_end(t):
+    r'\*/'
+    t.lexer.pop_state()
+
+t_ignore = '\t| '
+t_string_ignore = '\t'
+t_comment_ignore = '\t'
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+def t_string_error(t):
+    print("string: Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+def t_comment_error(t):
+    print("comment: Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+
+lexer = lex.lex()
+
+def lex_test():
+    f = open('main.c', 'r')
+    data = f.read()
+    f.close()
+
+    lexer.input(data)
+
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break
+        print(tok)
+
+if __name__ == '__main__':
+    lex_test()
---
+++ yacc.py
@@ -0,0 +1,230 @@
+import ply.yacc as yacc
+from lex import lexer
+from tokens import tokens
+
+
+start = 'program'
+
+def p_program(p):
+    '''
+    program : external_definitions
+    '''
+    p[0] = ['program', p[1]]
+
+def p_external_definitions(p):
+    '''
+    external_definitions : external_definition
+    | external_definitions external_definition
+    '''
+    if (len(p) == 2):
+        p[0] = [p[1]]
+    else:
+        p[0] = p[1] + [p[2]]
+
+
+
+def p_external_definition(p):
+    '''
+    external_definition : VAR SYMBOL SEMICOLON
+    | FUNC SYMBOL parameter_list block
+    | VAR SYMBOL ASSIGN expr SEMICOLON
+    '''
+    if (len(p) == 4):
+        #p[0] = "define var"
+        p[0] = ["globalvar", p[2], 0]
+    elif (len(p) == 5):
+        p[0] = ["func", p[3], p[4]]
+    else:
+        p[0] = ["globalvar", p[2], p[4]]
+
+def p_parameter_list(p):
+    '''
+    parameter_list : LPAREN RPAREN
+    | LPAREN symbol_list RPAREN
+    '''
+    if (len(p) == 3):
+        p[0] = []
+    if (len(p) == 4):
+        p[0] = p[2]
+
+def p_symbol_list(p):
+    '''
+    symbol_list : SYMBOL
+    | symbol_list COMMA SYMBOL
+    '''
+    if (len(p) == 2):
+        p[0] = [p[1]]
+    if (len(p) == 4):
+        p[0] = p[1] + [p[3]]
+
+def p_block(p):
+    '''
+    block : LBRACE statements RBRACE
+    '''
+    p[0] = p[2]
+
+
+def p_statements(p):
+    '''
+    statements : statement
+    | statements statement
+    '''
+    if (len(p) == 2):
+        p[0] = [p[1]]
+    else:
+        p[0] = p[1] + [p[2]]
+
+
+def p_statement(p):
+    '''
+    statement : expr SEMICOLON
+    | block
+    | local_vars
+    | RETURN expr SEMICOLON
+    | RETURN SEMICOLON
+    | IF LPAREN expr RPAREN statement
+    | IF LPAREN expr RPAREN statement ELSE statement
+    | WHILE LPAREN expr RPAREN statement
+    | FOR LPAREN expr SEMICOLON expr SEMICOLON expr RPAREN statement
+    '''
+    if (p[1] == 'return'):
+        if (len(p) == 3):
+            p[0] = ['return', None]
+        else:
+            p[0] = ['return', p[2]]
+    elif (p[1] == 'IF'):
+        if (len(p) == 6):
+            p[0] = ['IF', p[3], p[5]]
+        else:
+            p[0] = ['IFELSE', p[3], p[5], p[7]]
+    elif (p[1] == 'WHILE'):
+        p[0] = ['WHILE', p[3], p[5]]
+    elif (p[1] == 'for'):
+        p[0] = ['FOR', p[3], p[5], p[7], p[9]]
+    else:
+        p[0] = p[1];
+
+
+def p_local_vars(p):
+    '''
+    local_vars : VAR SYMBOL SEMICOLON
+    local_vars : VAR SYMBOL ASSIGN expr SEMICOLON
+    '''
+    if (len(p) == 4):
+        p[0] = ["localvar", p[2], 0]
+    else:
+        p[0] = ["localvar", p[2], p[4]]
+
+
+
+def p_expr(p):
+    '''
+    expr : primary_expr
+    | SYMBOL ASSIGN expr
+    | expr UNIOP
+    | expr BIOP expr
+    | expr TERNARY expr COLON expr
+    | OUTPUT LPAREN STRING COMMA expr RPAREN
+    '''
+    if (len(p) == 2):
+        p[0] = p[1]
+    elif (p[1] == 'output'):
+        p[0] = ['output', p[3], p[5]]
+    elif (p[2] == '='):
+        p[0] = ['assign', p[1], p[3]]
+    elif (p[2] == '++'):
+        p[0] = ['inc', p[1]]
+    elif (p[2] == '--'):
+        p[0] = ['dec', p[1]]
+    elif (p[2] == '!'):
+        p[0] = ['inv', p[1]]
+    elif (p[2] == '+'):
+        p[0] = ['add', p[1], p[3]]
+    elif (p[2] == '-'):
+        p[0] = ['sub', p[1], p[3]]
+    elif (p[2] == '*'):
+        p[0] = ['mul', p[1], p[3]]
+    elif (p[2] == '/'):
+        p[0] = ['div', p[1], p[3]]
+    elif (p[2] == '<'):
+        p[0] = ['lt', p[1], p[3]]
+    elif (p[2] == '<='):
+        p[0] = ['lte', p[1], p[3]]
+    elif (p[2] == '>'):
+        p[0] = ['gt', p[1], p[3]]
+    elif (p[2] == '>='):
+        p[0] = ['gte', p[1], p[3]]
+    elif (p[2] == '=='):
+        p[0] = ['eq', p[1], p[3]]
+    elif (p[2] == '!='):
+        p[0] = ['neq', p[1], p[3]]
+    elif (p[2] == '?'):
+        p[0] = ['ternary', p[1], p[3], p[5]]
+    else:
+        p[0] = p[1]
+
+
+def p_primary_expr(p):
+    '''
+    primary_expr : symbol
+    | number
+    | string
+    | SYMBOL LPAREN arg_list RPAREN
+    | SYMBOL LPAREN RPAREN
+    | INPUT LPAREN STRING RPAREN
+    '''
+    if (len(p) == 2):
+        p[0] = p[1]
+    elif (p[1] == "input"):
+        p[0] = ['input', p[3]]
+    else:
+        if (len(p) == 4):
+            p[0] = ['funcall', []]
+        else:
+            p[0] = ['funcall', p[3]]
+
+def p_symbol(p):
+    '''
+    symbol : SYMBOL
+    '''
+    p[0] = ['symbol', p[1]]
+
+def p_number(p):
+    '''
+    number : NUMBER
+    '''
+    p[0] = ['number', p[1]]
+
+def p_string(p):
+    '''
+    string : STRING
+    '''
+    p[0] = ['string', p[1]]
+
+
+def p_arg_list(p):
+    '''
+    arg_list : expr
+    | arg_list COMMA expr
+    '''
+    if (len(p) == 2):
+        p[0] = [p[1]]
+    else:
+        p[0] = p[1] + [p[3]]
+
+
+def p_error(p):
+    print("Syntax error at '%s'" % p)
+
+if __name__  == '__main__':
+
+    parser = yacc.yacc()
+
+    f = open('main.c', 'r')
+    data = f.read()
+    f.close()
+
+    ast = yacc.parse(data, lexer=lexer)
+    print(ast)
+
+

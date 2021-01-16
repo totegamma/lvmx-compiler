9ced9abc2afc044f369b0a108027a4ca1cf02600
thotgamma <github@gammalab.net>
2021-01-16 16:20:06 +0900

    Update lvmx.h

--- lvmxlib/lvmx.h
+++ lvmxlib/lvmx.h
@@ -2,11 +2,12 @@
 #define LVMX_H
 
 #define WORLDROOT_SLOT 0
-#define UIXROOT_SLOT 1
+#define DEVICEROOT_SLOT 1
+#define UIXROOT_SLOT 2
 
 #define debuglog(message) __raw(void, "PRINT", 0, message)
 #define createSlotFromTemplate(templateName) __raw(int, "CSFT", 0, templateName)
 #define setSlotParent(targetSlotID, newParentSlotID) __raw(void, "SSPA", 0, targetSlotID, newParentSlotID)
-#define setDVs(destSlotID, key, value) __raw(void, "WRIDS", 0, destSlotID, key, value)
+#define setDVString(destSlotID, key, value) __raw(void, "STORED", 2, destSlotID, key, value)
 
 #endif
